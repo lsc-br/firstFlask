a009e2b89076313968404685eefe98a49eacaf3a
Lucas <lsc.contas@protonmail.com>
2023-03-30 21:05:45 -0300

    first commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,47 @@
+import os
+from flask import Flask
+
+
+# 1 The Application Factory¶ - create_app()
+def create_app(test_config=None):
+    # create and configure app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, Flask!'
+
+    # //--\\ (1.0) - Application Setup¶
+    # \\--// (2.4) - Define and Access the Database¶ -> Initialize the Database File¶
+
+    from . import db
+    db.init_app(app)
+
+    # //--\\ (2.4) - Define and Access the Database¶ -> Initialize the Database File¶
+    # \\--// (3.2) - Blueprints and Views¶ -> Import and register the blueprint from[,,,]
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    # //--\\ Blueprints and Views¶ -> Import and register the blueprint from[,,,]
+    # \\--// Adicionados 4
+
+    return app
---
+++ flaskr/auth.py
@@ -0,0 +1,104 @@
+# (3.0) - Create a Blueprint¶
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flask.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth') # This creates a Blueprint named 'auth'.
+
+
+# (3.3) - Create a Blueprint¶ -> The First View: Register¶
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+
+            else:
+                return redirect(url_for("auth.login"))
+            flash(error)
+        return render_template('auth/register.html')
+
+
+# (3.4) - Create a Blueprint¶ -> Login¶
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+# (3.5) - bp.before_app_request() registers a function that runs before the view function, no matter what URLs requested
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+
+# (3.6) - Create a Blueprint¶ -> Logout¶
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+# (3.7) - Create a Blueprint¶ -> Require Authentication in Other Views¶
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+# (3.8) - Create a Blueprint¶ -> Endpoints and URLs¶
+    # TEXT EXPLANATION
